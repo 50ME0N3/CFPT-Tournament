ed2a0142e1105ff265d4f4cd8788838fb40159b7
Jérémie Arcidiacono <jxrx2004@gmail.com>
2022-03-21 14:05:37 +0100

    Add HTTPS on the site and remove HTTP support

--- site/route.js
+++ site/route.js
@@ -7,20 +7,27 @@ const axios = require('axios');
 const bp = require('body-parser')
 const config = require("./config.js");
 const logger = require('../logger.js').logger
+const fs = require('fs');
+const https = require('https');
+
+const options = {
+  key: fs.readFileSync(__dirname + '/ssl/privkey.pem'),
+  cert: fs.readFileSync(__dirname + '/ssl/cert.pem')
+};
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 app.use('/static', express.static(__dirname + '/static'));
 
-const storage = new JsonDatabase();
+console.log(__dirname);
+const storage = new JsonDatabase(__dirname + '/../db.json');
 const manager = new BracketsManager(storage);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/bracket.html");
 })
 
-app.route("/status")
-    .put(async (req,res) => {
+app.put("/status", async (req,res) => {
         let changedStatus = {
             id: parseInt(req.query.id),
             status: 6
@@ -28,64 +35,63 @@ app.route("/status")
         await manager.update.match(changedStatus)
         res.send("status changed")
     }) 
-app.route("/bracket")
-    .get(async (req, res) => {
+app.get("/bracket", async (req, res) => {
         const data = await manager.get.stageData(0);
         res.json(data);
-    })
-    .post((req, res) => {
-        if(req.body["API_KEY"] === config.API_KEY){
-            createTournament().then(r => "");
-            res.send("Brackets created")
-            logger.log('info','SITE - The bracket has been generated');
-        }
-        else{
-            logger.log('warn','SITE - A bracket creation has been attempted. But the API KEY was a fake one.');
-            res.send("wrong api key")
-        }
+})
+app.post("/bracket", (req, res) => {
+    if(req.body["API_KEY"] === config.API_KEY){
+        createTournament().then(r => "");
+        res.send("Brackets created")
+        logger.log('info','SITE - The bracket has been generated');
+    }
+    else{
+        logger.log('warn','SITE - A bracket creation has been attempted. But the API KEY was a fake one.');
+        res.send("wrong api key")
+    }
 
-    })
-    .delete((req, res) => {
-        manager.delete.stage(parseInt(req.body.id)).then(r => "")
-        manager.reset.seeding(parseInt(req.body.id)).then(r => "");
-        manager.reset.participant();
-        res.send("Brackets deleted");
-    })
-    .put(async (req, res) => {
-        let match = req.body
-        let updatedMatch
-        if(parseInt(match.scoreA) === 2){
-            updatedMatch = {
-                 id: parseInt(match.matchId),
-                opponent1: {
-                    score: parseInt(match.scoreA),
-                    result: 'win'
-                },
-                opponent2: {
-                    score: parseInt(match.scoreB),
-                    result: 'loss'
-                }
+})
+app.delete("bracket", (req, res) => {
+    manager.delete.stage(0).then(r => "")
+    manager.reset.seeding(0).then(r => "");
+    res.send("Brackets deleted");
+})
+app.put("/bracket", async (req, res) => {
+    let match = req.body
+    let updatedMatch
+    if(parseInt(match.scoreA) === 2){
+        console.log("vrai")
+        updatedMatch = {
+            id: parseInt(match.matchId),
+            opponent1: {
+                score: parseInt(match.scoreA),
+                result: 'win'
+            },
+            opponent2: {
+                score: parseInt(match.scoreB),
+                result: 'loss'
             }
         }
-        else{
-            updatedMatch = {
-                id: parseInt(match.matchId),
-                opponent1: {
-                    score: parseInt(match.scoreA),
-                    result: 'loss'
-                },
-                opponent2: {
-                    score: parseInt(match.scoreB),
-                    result: 'win'
-                }
+    }
+    else{
+        updatedMatch = {
+            id: parseInt(match.matchId),
+            opponent1: {
+                score: parseInt(match.scoreA),
+                result: 'loss'
+            },
+            opponent2: {
+                score: parseInt(match.scoreB),
+                result: 'win'
             }
         }
-        await manager.update.match(updatedMatch);
-        res.send("all good");
-    })
+    }
+    await manager.update.match(updatedMatch);
+    res.send("all good");
+})
 
 async function createTournament() {
-    const response = await axios.get("http://localhost:3000/teamsWithId", {})
+    const response = await axios.get("http://83.166.147.192:3000/teamsWithId", {})
         .then((res) => res.data)
         .catch((error) => console.error(error));
 
@@ -102,6 +108,6 @@ async function createTournament() {
     });
 }
 
-const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-});
+
+const serverHTTPS = https.createServer(options, app).listen(443);
+console.log(`Server running -> PORT 443`)
